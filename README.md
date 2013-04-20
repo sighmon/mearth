f4cefe51d128909c58d212480a63a0bf79ca3746
sighmon <simon.loffler@gmail.com>
2013-04-20 14:37:47 +0930

    Rails install.

--- README.md
+++ README.md
@@ -29,4 +29,5 @@ A 2013 NASA SpaceApps Challenge comparing the temperature, pressure & wind speed
 ## Space Apps Challenge
 
 Based on the challenge [Wish you were here](http://spaceappschallenge.org/challenge/wish-you-were-here/).
+
 Built by the team in Adelaide, Australia.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mearth::Application.config.secret_token = '1ef7e8bfe9df0bbdc8fb213859a183413012927f9591ce310c06b6530de4de13eb36914827053599334c8a601fec0b5d49eefed09ab9efca3c0a9821628234af'
